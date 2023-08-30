bd4f6a1f14a938e91929fa65aa12595af762cf06
Chris Drackett <chris@drackett.com>
2023-08-30 12:53:22 -0700

    test project for mmkv issue on expo 49

--- App.js
+++ App.js
@@ -1,10 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { MMKV } from 'react-native-mmkv'
+import Animated from 'react-native-reanimated'
+
+export const storage = new MMKV({
+  id: `storage`,
+})
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Date from MMKV: {storage.getString('date') || 'none set'}</Text>
+      <Button title="set date" onPress={() => {
+        storage.set('date', new Date().toLocaleString())
+      }} />
+      <Animated.View
+        style={{
+          width: 100,
+          height: 100,
+          backgroundColor: 'violet',
+        }}
+      />
       <StatusBar style="auto" />
     </View>
   );
